4b8bafe942ebf5bcf92a893972751a51240f54e3
Maxim Poletaev <max.poletaev@gmail.com>
2015-12-17 09:49:33 +0300

    Concurrency

--- project/counter/views.py
+++ project/counter/views.py
@@ -1,9 +1,27 @@
+from tornado.concurrent import run_on_executor, futures
 from tornado.web import RequestHandler
+from tornado.ioloop import IOLoop
+from tornado import gen
+from time import sleep
+
 from .models import Vote
 
 
+class TaskRunner:
+    def __init__(self, loop=None):
+        self.io_loop = loop or IOLoop.instance()
+        self.executor = futures.ThreadPoolExecutor(2)
+
+    @run_on_executor
+    def increment(self):
+        sleep(0.1)
+        return 'hello'
+
+
+tasks = TaskRunner()
+
 class CounterHandler(RequestHandler):
+    @gen.coroutine
     def get(self):
-        Vote.objects.create()
-        counter_value = len(Vote.objects.all())
-        self.write(str(counter_value))
+        message = yield tasks.increment()
+        self.write(message)
